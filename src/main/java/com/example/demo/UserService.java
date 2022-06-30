5402f3a695261e001a9150dc38edd4f87bc1e3eb
Femina <femina.abdulkhader@ibm.com>
2022-06-30 07:40:34 +0530

    test

--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -17,7 +17,7 @@ import java.util.List;
 		return users;
 	}
 
-	public List<String> getNewUsers2() {
+	public List<String> getNewUsers4() {
 
 		List<String> users = new ArrayList<>();
 		users.add("TEST1");
